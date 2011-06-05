51dbbf6d6af08588b31c584df8dca2988e9bb5fd
Mark Lewis <mark@casadelewis.com>
2011-06-05 18:43:29 -0400

    Added comment/reminder.

--- lib/class.ga.php
+++ lib/class.ga.php
@@ -1,10 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+	/**
+	* Thinking ahead for public release. Learn class implementation better!
+	*/
+
+	Class GoogleAnalytics {
+		
+		function __construct(){
+		}
+		
+		public function apiRequest($settings = array()) {
+		}
+		
+		public function curlRequest() {
+		}
+		
+		public function getSessionToken() {
+		}
+		
+	}
+
+?>
