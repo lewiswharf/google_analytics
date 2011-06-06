9790d4e3e28f118cf75f4432788caaa882ffa74c
Mark Lewis <mark@casadelewis.com>
2011-06-05 21:40:29 -0400

    Added top pages and top keywords to overview.

--- assets/google_analytics.index.js
+++ assets/google_analytics.index.js
@@ -1,11 +1,14 @@
-		google.load("visualization", "1", {packages:["corechart"]});
+		google.load("visualization", "1", {packages:["corechart", "table"]});
 
 		function drawChart() {
 			jQuery(document).ready(function() {
+				
+				var width = jQuery(window).width();
+				//Big chart
 				var data = new google.visualization.DataTable();
 						
-				var headers = jQuery(this).find('thead').find('th');
-				var rows = jQuery(this).find('tbody').find('tr');
+				var headers = jQuery('#thirty-day-chart').find('thead').find('th');
+				var rows = jQuery('#thirty-day-chart').find('tbody').find('tr');
 			
 				headers.each(function(index){
 					if(index){
@@ -29,15 +32,85 @@
 		
 				var chart = new google.visualization.AreaChart(document.getElementById('thirty-day-chart'));
 				chart.draw(data, {
-														width: jQuery(window).width(), 
+														width: width, 
+														chartArea:{width: width-120},
 														height: 240, 
 														lineWidth: 3,
 														pointSize: 5,
+														legend: 'top',
 														colors: ['#2A4269', '#38588E'],
 														hAxis: {slantedText: true, slantedTextAngle: 30, textStyle: {color: '#666666', fontSize: 12}, showTextEvery: 2},
 														vAxis: {textStyle: {color: '#666666', fontSize: 12}}
+												 });
+			
+				//Pages table
+				var data2 = new google.visualization.DataTable();
+						
+				var headers2 = jQuery('#top-pages').find('thead').find('th');
+				var rows2 = jQuery('#top-pages').find('tbody').find('tr');
+			
+				headers2.each(function(index){
+					if(index){
+							data2.addColumn('number',jQuery(this).text());
+					} else {
+							data2.addColumn('string',String(jQuery(this).text()));
+					}
+				});
+			
+				data2.addRows(rows2.length);
+			
+				rows2.each(function(index){
+						jQuery(this).find('td').each(function(index2){
+							if(index2){
+								data2.setCell(index, index2, parseFloat(jQuery(this).text()));
+							} else {
+								data2.setCell(index, index2, String(jQuery(this).text()));
+							}
+						});
+				});
+
+				var mytable = new google.visualization.Table(document.getElementById('top-pages'));
+				mytable.draw(data2, {
+														width: Math.round(width/2)-20,
+														showRowNumber: true,
+														page: 'enable', 
+												 });
+			
+				//Keywords table
+				var data3 = new google.visualization.DataTable();
+						
+				var headers3 = jQuery('#top-keywords').find('thead').find('th');
+				var rows3 = jQuery('#top-keywords').find('tbody').find('tr');
+			
+				headers3.each(function(index){
+					if(index){
+							data3.addColumn('number',jQuery(this).text());
+					} else {
+							data3.addColumn('string',String(jQuery(this).text()));
+					}
+				});
+			
+				data3.addRows(rows3.length);
+			
+				rows3.each(function(index){
+						jQuery(this).find('td').each(function(index2){
+							if(index2){
+								data3.setCell(index, index2, parseFloat(jQuery(this).text()));
+							} else {
+								data3.setCell(index, index2, String(jQuery(this).text()));
+							}
+						});
+				});
+
+				var mytable2 = new google.visualization.Table(document.getElementById('top-keywords'));
+				mytable2.draw(data3, {
+														width: Math.round(width/2)-20,
+														allowHtml: true,
+														showRowNumber: true,
+														page: 'enable', 
 												 });
 			});
 		}
 			
 		google.setOnLoadCallback(drawChart);
+
--- content/content.index.php
+++ content/content.index.php
@@ -33,10 +33,40 @@
 					
 				$xml = $this->_driver->curlRequest($feed, $this->_driver->getSessionToken());
 //				die($xml);
-				$xsl = file_get_contents(EXTENSIONS . '/google_analytics/utilities/report.xsl');
+				$xsl = file_get_contents(EXTENSIONS . '/google_analytics/utilities/report.chart.xsl');
 				$output = new XMLElement("div", $this->_driver->transformDataFeedWithXSLT($xsl, $xml));
 				$output->setAttribute("id", "ga-index");			
 				
+				$feed2 = self::GA_REPORT_DATA
+				  . '?ids=' . $profile 
+					. '&start-date=' . DateTimeObj::format('-1 month', 'Y-m-d') 
+					. '&end-date=' . DateTimeObj::format('now', 'Y-m-d')
+    			. '&dimensions=ga:pageTitle' 
+    			. '&metrics=ga:pageviews' 
+    			. '&sort=-ga:pageviews';
+					
+				$xml2 = $this->_driver->curlRequest($feed2, $this->_driver->getSessionToken());
+//				print_r($xml2);
+				$xsl2 = file_get_contents(EXTENSIONS . '/google_analytics/utilities/report.pages.xsl');
+				$output2 = new XMLElement("div", $this->_driver->transformDataFeedWithXSLT($xsl2, $xml2));
+				
+				$output->appendChild($output2);
+				
+				$feed3 = self::GA_REPORT_DATA
+				  . '?ids=' . $profile 
+					. '&start-date=' . DateTimeObj::format('-1 month', 'Y-m-d') 
+					. '&end-date=' . DateTimeObj::format('now', 'Y-m-d')
+    			. '&dimensions=ga:keyword' 
+    			. '&metrics=ga:visitors' 
+    			. '&sort=-ga:visitors';
+					
+				$xml3 = $this->_driver->curlRequest($feed3, $this->_driver->getSessionToken());
+//				die($xml3);
+				$xsl3 = file_get_contents(EXTENSIONS . '/google_analytics/utilities/report.keywords.xsl');
+				$output3 = new XMLElement("div", $this->_driver->transformDataFeedWithXSLT($xsl3, $xml3));
+				
+				$output->appendChild($output3);
+
 				$this->Form->appendChild($output);
 			} else {
 				redirect(URL . 'symphony/extension/google_analytics/index/getprofile/');
